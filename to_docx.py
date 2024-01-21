e3bedbb711eb9aadc73e381e610ec4d8af8c5acd
Frank Liang <frankliang@franks-mbp.home>
2024-01-21 15:29:43 +0000

    Better workflow with document files

--- to_docx.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-from htmldocx import HtmlToDocx
-
-new_parser = HtmlToDocx()
-
-input_path='Fanzhou_Liang.html'
-output_path='Fanzhou_Liang.docx'
-
-new_parser.parse_html_file(input_path, output_path)
